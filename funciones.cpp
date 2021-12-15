68854f4526cd6d1d668ae7893c828946f8bde0bc
maurialv1401 <83974344+maurialv1401@users.noreply.github.com>
2021-12-14 21:05:27 -0500

    Update funciones.cpp

--- funciones.cpp
+++ funciones.cpp
@@ -4,32 +4,54 @@
 #include <vector>
 #include <algorithm>
 #include "funciones.h"
-
+#include "board.h"
 using namespace std;
 
 
-vector<string> letters = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+const vector<string> letters = {"A","B","C","D","E","F","G","H","I","J"};
+const vector<string> nums = {"1","2","3","4","5","6","7","8","9","10"};
+vector<string> shots;
 
-vector<string> split(const string &s, char delim) {
-    vector<string> result;
-    stringstream ss(s);
-    string item;
-    while (getline(ss, item, delim)) {
-        result.push_back(item);
-    }
+vector<string> split (const string &s, char delim) {
+  vector<string> result;
+  stringstream ss (s);
+  string item;
+  while (getline (ss, item, delim)) {
+    result.push_back (item);
+  }
 
-    return result;
+  return result;
 }
 
-template<typename T>
-pair<bool, int> findInVector(const vector<T> &vecOfElements, const T &element) {
-    pair<bool, int> result;
+void save_pos(string pos){
+  shots.push_back(pos);
+}
+void show_vec(){
+  for(auto c: shots) cout<<c<<endl;
+}
+
+bool find_vec(string position){
+  for (auto coord: shots){
+    if (coord == position)
+      return true;
+    return false;
+  }
+    
+}
+
+template < typename T>
+std::pair<bool, int > findInVector(const std::vector<T>  & vecOfElements, const T  & element)
+{
+    std::pair<bool, int > result;
     // Find given element in vector
-    size_t it = find(vecOfElements.begin(), vecOfElements.end(), element);
-    if (it != vecOfElements.end()) {
+    auto it = std::find(vecOfElements.begin(), vecOfElements.end(), element);
+    if (it != vecOfElements.end())
+    {
         result.second = distance(vecOfElements.begin(), it);
         result.first = true;
-    } else {
+    }
+    else
+    {
         result.first = false;
         result.second = -1;
     }
@@ -37,11 +59,16 @@ pair<bool, int> findInVector(const vector<T> &vecOfElements, const T &element) {
 }
 
 
-/*
-Implementacion
-std::pair<bool, int> result = findInVector<int>(vecOfNums, 45);
-if (result.first)
-  std::cout << "Element Found at index : " << result.second <<std::endl;
-else
-  std::cout << "Element Not Found" << std::endl;
-*/
+string pos_to_string(int r, int c){
+  string res = letters[r]+nums[c];
+  return res;
+}
+
+
+string random_pos(){
+  srand(time(NULL));
+  int n1=rand()%10;
+  int n2=rand()%10;
+  string res = letters[n1]+nums[n2];
+  return res;
+}
